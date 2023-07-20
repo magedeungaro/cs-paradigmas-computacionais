b1126478a95bce6241c89ea73b1dc93f0a1b96c3
magedeungaro <magedbgt@gmail.com>
2023-07-20 00:55:21 -0300

    prime number

---
+++ exercises/prime_number.py
@@ -0,0 +1,24 @@
+def is_prime_iterative(number):
+    if number <= 1:
+        return False;
+
+    for i in range(2, number):
+        if number % i == 0:
+            return False;
+    return True;
+
+def run_prompt():
+    number = int(input('Enter an integer number: '));
+    return number;
+
+def result_message(is_prime):
+    print_value = 'prime' if is_prime else 'not prime';
+    return f"This number is {print_value}";
+
+def main():
+    number = run_prompt();
+    result = is_prime_iterative(number);
+    print(result_message(result));
+
+main();
+
