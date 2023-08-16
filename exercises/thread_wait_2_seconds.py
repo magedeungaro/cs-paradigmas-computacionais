88b355224c2c307c67f0150f386177ca123756a0
magedeungaro <magedbgt@gmail.com>
2023-08-15 22:08:33 -0300

    ajustes exercício - display de miliseconds e uso de datetime

--- exercises/thread_wait_2_seconds.py
+++ exercises/thread_wait_2_seconds.py
@@ -1,16 +1,21 @@
 from threading import Thread
 from time import sleep
-import time
+from datetime import datetime
+
+def formatted_time():
+    return datetime.now().strftime("%a %b %d %Y %H:%M:%S.%f")
 
 def print_time():
-    print(f"Started at: {time.ctime()}")
+    print(f"Task started at: {formatted_time()}")
     sleep(2)
-    print(f"Finished at: {time.ctime()}")
+    print(f"Task Finished at: {formatted_time()}")
 
 def main():
     t1 = Thread(target=print_time)
     t1.start()
+    print("Waiting for thread to finish")
     t1.join()
+    print("Thread finished at: ", formatted_time())
 
 if __name__ == '__main__':
     main()
